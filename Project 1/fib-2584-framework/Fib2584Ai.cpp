21f27fc5f67987486837be00bc2b742cbd470a62
stream <stream@localhost.localdomain>
2016-10-15 05:57:35 -0400

    first commit

---
+++ Project 1/fib-2584-framework/Fib2584Ai.cpp
@@ -0,0 +1,98 @@
+#include "Fib2584Ai.h"
+#include "Fib2584/GameBoard.h"
+#include "Fib2584/MoveDirection.h"
+#include "Fib2584/GameBoardte.h"
+#include "Fib2584/BitBoard.h"
+#include <algorithm>
+#include <iterator>
+int Fib2584Ai::fibonacci_[32] = {0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309};
+Fib2584Ai::Fib2584Ai()
+{
+}
+
+void Fib2584Ai::initialize(int argc, char* argv[])
+{
+	srand(time(NULL));
+	return;
+}
+
+BitBoard Fib2584Ai::parseArray(int board[4][4]){
+	unsigned long long q=0;
+	BitBoard parse= BitBoard(q);
+	for(int i = 15;i >=0 ;i--) {
+		auto result =find(fibonacci_,fibonacci_+31,board[3-(i/4)][3-(i%4)]);
+		int idx=std::distance( std::begin( fibonacci_ ), result );
+		/*
+		cout<<idx<<" ";
+		if(i%4==0){
+		    cout<<endl;
+		}*/
+		BitBoard temp=BitBoard(idx);
+		temp<<=5*i;
+		parse|=temp;
+	}
+	return parse;
+}
+
+MoveDirection Fib2584Ai::generateMove(int board[4][4])
+{
+	MoveDirection Move= SerachTree(board,3);
+	return Move;
+}
+
+void Fib2584Ai::gameOver(int board[4][4], int iScore)
+{
+	return;
+}
+
+MoveDirection Fib2584Ai::SerachTree(int board[4][4],int iteration)
+{
+	MoveDirection randomMove = static_cast<MoveDirection>(rand() % 4);
+	
+	GameBoardte gb;
+	BitBoard parse= parseArray(board);
+	gb.board_=parse;
+	int simuScore[4]={0};
+	int temp_simu=0;
+	int temp_count=0;
+	for(int i =0;i<4;i++){
+		BitBoard parse_temp=parse;
+		gb.board_=parse_temp;
+		MoveDirection TrialMove = static_cast<MoveDirection>(i);
+		GameBoardte originalBoard = gb;
+		simuScore[i]+=gb.move(TrialMove);
+		
+		if(originalBoard == gb){
+			continue;
+		}
+		gb.addRandomTile();
+		for (int ix=0;ix<iteration;ix++){
+			int temp_board[4][4];
+			gb.getArrayBoard(temp_board);	
+			MoveDirection Move= SerachTree(temp_board,iteration-1);
+			GameBoardte originalBoard = gb;
+			simuScore[i] += gb.move(Move);
+			if(originalBoard == gb){
+				break;
+			}
+			if(!gb.terminated()){
+				gb.addRandomTile();
+			}else{
+				break;
+			}
+		}
+		
+		if(simuScore[i]>temp_simu){
+			randomMove=TrialMove;
+			temp_simu=simuScore[i];
+			temp_count=i;
+			//cout<<"score is : "<< temp_simu<<endl;
+		}
+	}
+	//cout<<"action is : "<< temp_count<<endl;
+	return randomMove;
+}
+/**********************************
+You can implement any additional functions
+you may need.
+**********************************/
---
+++ Project 1/fib-2584-framework/Fib2584Ai.h
@@ -0,0 +1,31 @@
+#ifndef __FIB2584AI_H__
+#define __FIB2584AI_H__
+
+#include <cstdlib>
+#include <ctime>
+#include "Fib2584/MoveDirection.h"
+#include "Fib2584/BitBoard.h"
+
+class Fib2584Ai
+{
+public:
+	Fib2584Ai();
+	// initialize ai
+	void initialize(int argc, char* argv[]);
+	// generate one move
+	MoveDirection generateMove(int board[4][4]);
+	// do some action when game over
+	void gameOver(int board[4][4], int iScore);
+
+	BitBoard parseArray(int board[4][4]);	
+	MoveDirection SerachTree(int board[4][4],int iteration);	
+	MoveDirection NSerachTree(int board[4][4],int iteration);	
+
+	static int fibonacci_[32];
+	/**********************************
+	You can implement any additional functions
+	or define any variables you may need.
+	**********************************/
+};
+
+#endif
